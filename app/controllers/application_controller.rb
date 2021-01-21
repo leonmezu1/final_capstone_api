6cc7383b7682b191c4c4281b9d03eec4bddaf358
leonmezu1 <leo.mez@outlook.com>
2021-01-21 10:39:24 -0500

    Added create action for registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include JsonErrors
+
+  def render_created_user(user)
+    payload = { user_id: user.id }
+    token = encode_token payload
+    render json: {
+      status: :created,
+      logged_in: true,
+      user: UserSerializer.new(user).serializable_hash,
+      jwt: token
+    }
+  end
+
+  private
+
+  def encode_token(payload = {})
+    exp = 24.hours.from_now
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.credentials.token_encoder)
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,15 @@
 class RegistrationsController < ApplicationController
-  def create; end
+  def create
+    user = User.create!(user_params)
+
+    return unless user.valid?
+
+    render_created_user user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
